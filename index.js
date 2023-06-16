0face05c45b6e7d16c99f0fd1df42078a84eed02
LapTrinhBlockchain <12036697+laptrinhbockchain@users.noreply.github.com>
2023-06-16 16:15:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var serveStatic = require('serve-static');
 var app = express();
 
 let port = 8888;
-// let appDir = "pancake";
-let appDir = "heraswap";
+let appDir = "pancake";
+// let appDir = "heraswap";
 app.use(serveStatic(appDir, { index: ['index.html'] }))
 app.listen(port);
 console.log("The app listen on port:", port);
